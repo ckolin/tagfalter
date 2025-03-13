6cf555a495dba0d6525addae49d0971f852731cc
ckolin <25738463+ckolin@users.noreply.github.com>
2025-03-13 20:18:32 +0100

    Fix build directory

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from "@sveltejs/adapter-static";
 
 export default {
 	kit: {
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
+			pages: "docs",
+			assets: "docs",
 			fallback: undefined,
 			precompress: false,
 			strict: true
